6e363182912d49e7b4bd339301ca18abe934b692
David Christian <david.w.christian@gmail.com>
2024-11-30 10:43:54 -0800

    Added path aliases

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test, describe, beforeEach } from 'bun:test';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@src/app.controller';
+import { AppService } from '@src/app.service';
 
 describe('AppController', () => {
 	let appController: AppController;
